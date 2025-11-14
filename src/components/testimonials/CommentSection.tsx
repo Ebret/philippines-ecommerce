dcb9746559d5a3afdded16b203507be301797549
Aurex Platform <aaurexgoldxx@gmail.com>
2025-11-14 22:05:51 +0800

    Fix build errors: Add textarea UI component and remove date-fns imports

--- src/components/testimonials/CommentSection.tsx
+++ src/components/testimonials/CommentSection.tsx
@@ -6,7 +6,6 @@
 'use client';
 
 import { useState } from 'react';
-import { formatDistanceToNow } from 'date-fns';
 
 interface Comment {
   id: string;
--- src/components/testimonials/TestimonialCard.tsx
+++ src/components/testimonials/TestimonialCard.tsx
@@ -7,7 +7,6 @@
 
 import Link from 'next/link';
 import Image from 'next/image';
-import { formatDistanceToNow } from 'date-fns';
 
 interface TestimonialCardProps {
   id: string;
